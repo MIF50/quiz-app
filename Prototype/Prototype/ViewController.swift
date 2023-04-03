3ea8c7bba603a915ed2d201026d448204a65bbd3
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-04-03 03:47:31 +0300

    Create a fast demo prototype for what i will build to get idea and feedback so i can use it when start coding

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Mohamed Ibrahim on 03/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
