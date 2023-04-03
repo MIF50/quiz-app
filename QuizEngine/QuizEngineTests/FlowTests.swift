ec9edb335dcf4e557246d809c723482e51ab08ae
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-04-03 22:25:52 +0300

    Start twice routes to first question twice

--- QuizEngine/QuizEngineTests/FlowTests.swift
+++ QuizEngine/QuizEngineTests/FlowTests.swift
@@ -16,25 +16,16 @@ final class FlowTests: XCTestCase {
         
         sut.start()
         
-        XCTAssertEqual(router.routedQuestionCount, 0)
-    }
-    
-    func test_start_withOneQuestion_routesToQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions: ["Q1"],router: router)
-
-        sut.start()
-
-        XCTAssertEqual(router.routedQuestionCount, 1)
+        XCTAssertTrue(router.routedQuestions.isEmpty)
     }
     
     func test_start_withOneQuestion_routesToCorrectQuestion() {
         let router = RouterSpy()
-        let sut = Flow(questions: ["Q1"],router: router)
+        let sut = Flow(questions: ["Q2"],router: router)
 
         sut.start()
 
-        XCTAssertEqual(router.routedQuestion, "Q1")
+        XCTAssertEqual(router.routedQuestions, ["Q2"])
     }
     
     func test_start_withTwoQuestions_routesToFirstQuestion() {
@@ -43,19 +34,27 @@ final class FlowTests: XCTestCase {
 
         sut.start()
 
-        XCTAssertEqual(router.routedQuestion, "Q1")
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+    
+    func test_startTwice_withTwoQuestions_routesToFirstQuestionTwice() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"],router: router)
+        
+        sut.start()
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1","Q1"])
     }
 
     
     //MARK: - Helper
     
     private class RouterSpy: Router {
-        var routedQuestionCount = 0
-        var routedQuestion: String? = nil
+        var routedQuestions = [String]()
         
         func routeTo(question: String) {
-            routedQuestionCount += 1
-            routedQuestion = question
+            routedQuestions.append(question)
         }
     }
 
