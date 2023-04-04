9fac1345cf0eca7f790485f11b4146af57c67a99
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-04-05 01:02:29 +0300

    Move duplicate code into makeSUT

--- QuizApp/QuizAppTests/QuestionViewControllerTests.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTests.swift
@@ -13,8 +13,6 @@ final class QuestionViewControllerTests: XCTestCase {
     func test_outlets_shouldBeConnected() {
         let sut = makeSUT()
         
-        sut.loadViewIfNeeded()
-        
         XCTAssertNotNil(sut.headerLabel, "headerLabel")
         XCTAssertNotNil(sut.tableView,"tableView")
     }
@@ -22,15 +20,11 @@ final class QuestionViewControllerTests: XCTestCase {
     func test_viewDidLoad_hasHeaderText() {
         let sut = makeSUT(question: "Q1")
         
-        sut.loadViewIfNeeded()
-        
         XCTAssertEqual(sut.textHeaderLabel, "Q1")
     }
     
     func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
         let sut = makeSUT(options: [])
-        
-        sut.loadViewIfNeeded()
         
         XCTAssertEqual(sut.numberOfOptions, 0)
     }
@@ -38,22 +32,17 @@ final class QuestionViewControllerTests: XCTestCase {
     func test_tableView_delegate_shouldBeConnected() {
         let sut = makeSUT()
         
-        sut.loadViewIfNeeded()
-        
         XCTAssertNotNil(sut.tableView.dataSource,"dataSource")
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOption() {
         let sut = makeSUT(options: ["A1"])
         
-        sut.loadViewIfNeeded()
-        
         XCTAssertEqual(sut.numberOfOptions, 1)
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOptionText() {
         let sut = makeSUT(options: ["A1"])
-        sut.loadViewIfNeeded()
         
         let cell = sut.optionView(at: 0)
         
@@ -64,6 +53,7 @@ final class QuestionViewControllerTests: XCTestCase {
     
     private func makeSUT(question: String = "",options: [String] = []) -> QuestionViewController {
         let sut = QuestionViewController(question: question,options: options)
+        sut.loadViewIfNeeded()
         return sut
     }
 
