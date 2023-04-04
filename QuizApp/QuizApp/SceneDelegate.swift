d846b288434ae123f4d1d10ead224e4d23ed3cb5
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-04-04 23:53:12 +0300

    Create empty iOS app

---
+++ QuizApp/QuizApp/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  QuizApp
+//
+//  Created by Mohamed Ibrahim on 04/04/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
