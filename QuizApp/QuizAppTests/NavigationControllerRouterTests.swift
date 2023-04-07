af9cf6b881754eef1f169dab2859322ba9f7b716
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-04-07 17:55:26 +0300

    Route to second question present second view controller

--- QuizApp/QuizAppTests/NavigationControllerRouterTests.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTests.swift
@@ -14,13 +14,32 @@ final class NavigationControllerRouterTests: XCTestCase {
     func test_routeToQuestion_presentsQuestionController() {
         let viewController = UIViewController()
         let factory = ViewControllerFactoryStub()
-        factory.stub(question: "Q1",with: viewController )
+        factory.stub(question: "Q1",with: viewController)
         let naviagtionController = UINavigationController()
         let sut = NavigationControllerRouter(naviagtionController,factory: factory)
         
         sut.routeTo(question: "Q1", answerCallback: { _ in })
         
+        XCTAssertEqual(naviagtionController.viewControllers.count, 1)
         XCTAssertEqual(naviagtionController.viewControllers.first, viewController)
+    }
+    
+    func test_routeToSeconQuestion_presentsQuestionController() {
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        let factory = ViewControllerFactoryStub()
+        factory.stub(question: "Q1",with: viewController)
+        factory.stub(question: "Q2",with: secondViewController)
+
+        let naviagtionController = UINavigationController()
+        let sut = NavigationControllerRouter(naviagtionController,factory: factory)
+        
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        sut.routeTo(question: "Q2", answerCallback: { _ in })
+        
+        XCTAssertEqual(naviagtionController.viewControllers.count, 2)
+        XCTAssertEqual(naviagtionController.viewControllers.first, viewController)
+        XCTAssertEqual(naviagtionController.viewControllers.last, secondViewController)
     }
     
     // MARK: - Helpers
